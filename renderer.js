6a4580601150f9cc6d7ddc1fe06ea56963039d82
XIAO Zhen <eric.xiaozhen@foxmail.com>
2017-11-12 21:46:44 +0800

    initial
    initial

---
+++ renderer.js
@@ -0,0 +1,35 @@
+﻿// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+(function(){
+  console.log("render.js:Hello world!");
+  const $ = require('jquery');
+  const ipc = require('electron').ipcRenderer;
+  var fs = require("fs");
+  var textEncoding = require('text-encoding'); 
+  var TextDecoder = textEncoding.TextDecoder;
+
+  $("#openfile").click(function(event){
+    console.log('clicked at openfile');
+    ipc.send('open-file-dialog');
+  });
+
+  ipc.on('selected-directory', function (event, record) {
+    $('#selected-file').text("filename:" + record.filename);
+
+    for(let i = 0; i < record.length; i ++){
+      $('#header').append($("<th/>").text(record[i]));
+    }
+    console.log(Object.prototype.toString.call(record[0]));
+    $('#selected-file').text(record.filename);
+  });
+
+  //Load album file
+  var readfile = function(record){
+
+  }
+
+
+
+
+})();
